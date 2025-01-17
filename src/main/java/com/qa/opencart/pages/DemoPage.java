d62bbf251dddab2ab8e7025b71ac04d7ffa9309d
PrakruthiS2 <prakruthisnadig@gmail.com>
2025-01-17 06:24:17 +0530

    added demo pagelatest

--- src/main/java/com/qa/opencart/pages/DemoPage.java
+++ src/main/java/com/qa/opencart/pages/DemoPage.java
@@ -5,11 +5,19 @@ import org.openqa.selenium.By;
 public class DemoPage {
 
 	By demo = By.id("username");
+	By cart = By.id("cart");
 
 	public void demo() {
 		// TODO Auto-generated method stub
 
 		System.out.println(demo);
+
+	}
+	
+	public void cart() {
+		// TODO Auto-generated method stub
+
+		System.out.println(cart);
 
 	}
 
